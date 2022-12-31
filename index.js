595c0df9a8f53f0fa932b48c5697efe1a1a0b0c3
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2022-12-31 14:41:55 +0000

    bg is changed

---
+++ index.js
@@ -0,0 +1,32 @@
+let metricInput = document.querySelector(".metric__input")
+let length = document.querySelector("#length")
+let volume = document.querySelector("#volume")
+let mass = document.querySelector("#mass")
+
+
+let meterToFeet = metricInput.value*3.28084
+let feetToMeter = metricInput.value*0.3048
+
+let litterToGallon = metricInput.value*0.219969
+let gallonToLitter = metricInput.value*4.54609
+
+let kgToPound = metricInput.value*2.20462
+let poundToKg = metricInput.value*0.453592
+
+length.textContent = `${metricInput.value} meters = ${meterToFeet.toFixed(3)} feet | ${metricInput.value} feet = ${feetToMeter.toFixed(3)} meters`
+volume.textContent = `${metricInput.value} liters = ${litterToGallon.toFixed(3)} gallons | ${metricInput.value} gallons = ${gallonToLitter.toFixed(3)} liters`
+mass.textContent = `${metricInput.value} kilos = ${kgToPound.toFixed(3)} pounds | ${metricInput.value} pounds = ${poundToKg.toFixed(3)} kilos`
+
+  
+metricInput.addEventListener("input", function(e){
+  let meterToFeet = metricInput.value*3.28084
+  let feetToMeter = metricInput.value*0.3048
+  let litterToGallon = metricInput.value*0.219969
+  let gallonToLitter = metricInput.value*4.54609
+  let kgToPound = metricInput.value*2.20462
+  let poundToKg = metricInput.value*0.453592
+
+  length.textContent = `${e.target.value} meters = ${meterToFeet.toFixed(3)} feet | ${e.target.value} feet = ${feetToMeter.toFixed(3)} meters`
+  volume.textContent = `${e.target.value} liters = ${litterToGallon.toFixed(3)} gallons | ${e.target.value} gallons = ${gallonToLitter.toFixed(3)} liters`
+  mass.textContent = `${e.target.value} kilos = ${kgToPound.toFixed(3)} pounds | ${e.target.value} pounds = ${poundToKg.toFixed(3)} kilos`
+})
