98fb0604b1b138609135739dc0de0fb4f3f75315
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-01-01 01:22:01 +0000

    reasigning value

--- index.js
+++ index.js
@@ -13,23 +13,28 @@ let gallonToLitter = metricInput.value*4.54609
 let kgToPound = metricInput.value*2.20462
 let poundToKg = metricInput.value*0.453592
 
-length.textContent = `${metricInput.value} meters = ${meterToFeet.toFixed(3)} feet | ${metricInput.value} feet = ${feetToMeter.toFixed(3)} meters`
-volume.textContent = `${metricInput.value} liters = ${litterToGallon.toFixed(3)} gallons | ${metricInput.value} gallons = ${gallonToLitter.toFixed(3)} liters`
-mass.textContent = `${metricInput.value} kilos = ${kgToPound.toFixed(3)} pounds | ${metricInput.value} pounds = ${poundToKg.toFixed(3)} kilos`
+
+  length.textContent = `${metricInput.value} meters = ${meterToFeet.toFixed(3)} feet | ${metricInput.value} feet = ${feetToMeter.toFixed(3)} meters`
+  volume.textContent = `${metricInput.value} liters = ${litterToGallon.toFixed(3)} gallons | ${metricInput.value} gallons = ${gallonToLitter.toFixed(3)} liters`
+  mass.textContent = `${metricInput.value} kilos = ${kgToPound.toFixed(3)} pounds | ${metricInput.value} pounds = ${poundToKg.toFixed(3)} kilos`
 
   
-metricInput.addEventListener("input", function(e){
-  let meterToFeet = metricInput.value*3.28084
-  let feetToMeter = metricInput.value*0.3048
-  let litterToGallon = metricInput.value*0.219969
-  let gallonToLitter = metricInput.value*4.54609
-  let kgToPound = metricInput.value*2.20462
-  let poundToKg = metricInput.value*0.453592
+metricInput.addEventListener("input", function(){
+  
+  meterToFeet = metricInput.value*3.28084
+  feetToMeter = metricInput.value*0.3048
+  litterToGallon = metricInput.value*0.219969
+  gallonToLitter = metricInput.value*4.54609
+  kgToPound = metricInput.value*2.20462
+  poundToKg = metricInput.value*0.453592
+  
+  length.textContent = `${metricInput.value} meters = ${meterToFeet.toFixed(3)} feet | ${metricInput.value} feet = ${feetToMeter.toFixed(3)} meters`
+  volume.textContent = `${metricInput.value} liters = ${litterToGallon.toFixed(3)} gallons | ${metricInput.value} gallons = ${gallonToLitter.toFixed(3)} liters`
+  mass.textContent = `${metricInput.value} kilos = ${kgToPound.toFixed(3)} pounds | ${metricInput.value} pounds = ${poundToKg.toFixed(3)} kilos`
 
-  length.textContent = `${e.target.value} meters = ${meterToFeet.toFixed(3)} feet | ${e.target.value} feet = ${feetToMeter.toFixed(3)} meters`
-  volume.textContent = `${e.target.value} liters = ${litterToGallon.toFixed(3)} gallons | ${e.target.value} gallons = ${gallonToLitter.toFixed(3)} liters`
-  mass.textContent = `${e.target.value} kilos = ${kgToPound.toFixed(3)} pounds | ${e.target.value} pounds = ${poundToKg.toFixed(3)} kilos`
 })
+
+
 
 const checkbox = document.getElementById('checkbox')
 
